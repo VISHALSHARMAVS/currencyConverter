7c2f9e5c499c64a989dc9d4e636d73664412efc5
VISHALSHARMAVS <vishalsharmakt143@gamil.com>
2024-03-10 02:13:34 +0530

    Custom Hooks

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+      <h1 className='text-3xl font-bold underline justify-center'>Vishal Sharma </h1>
+    </>
+  )
+}
+
+export default App
